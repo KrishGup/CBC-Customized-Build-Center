bab0e674ffa523e34c0f0d7048e91779c41776bb
KrishGup <krishgup1@gmail.com>
2024-11-09 15:01:56 -0500

    Remove sudo from package update and add OpenSCAD accessibility check

--- CBC-API/build.sh
+++ CBC-API/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Updating package list..."
-sudo apt-get update
+apt-get update
 echo "Making OpenSCAD AppImage executable..."
 chmod +x ./bin/openscad.AppImage
+echo "Checking OpenSCAD Accessible"
+./bin/openscad.AppImage -v
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
