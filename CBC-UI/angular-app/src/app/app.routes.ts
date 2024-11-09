58a3bd3ae3a0bea3e9309cf3a03033b83e3c905b
KrishGup <krishgup1@gmail.com>
2024-11-09 00:24:28 -0500

    Add OrderStatusComponent and routing; include basic template and styles

--- CBC-UI/angular-app/src/app/app.routes.ts
+++ CBC-UI/angular-app/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OrderStatusComponent } from './order-status/order-status.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'order', component: OrderStatusComponent },
 ];
