a0b4c55a58aa44dd7bf1b4a5d1353b74bef29d46
Trexdog319 <nbusedu@gmail.com>
2024-11-08 23:01:01 -0500

    Remove routing configuration and update main component HTML

--- CBC-UI/angular-app/src/app/app.routes.ts
+++ CBC-UI/angular-app/src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { AppComponent as MainComponent } from './pages/main/app.component';
-import { AppComponent as MissionComponent } from './pages/mission/app.mission';
 
-export const routes: Routes = [
-{ path: '', component: MainComponent },
-  { path: 'mission', component: MissionComponent }
-];
+export const routes: Routes = [];
