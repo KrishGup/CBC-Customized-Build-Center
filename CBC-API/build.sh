8df99d2b82317c73b04185a01a4cf753b5f7e114
KrishGup <krishgup1@gmail.com>
2024-11-09 15:05:06 -0500

    Install libfuse2 for OpenSCAD AppImage execution

--- CBC-API/build.sh
+++ CBC-API/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Updating package list..."
 apt-get update
+echo "Installing libfuse..."
+apt-get install -y libfuse2
 echo "Making OpenSCAD AppImage executable..."
 chmod +x ./bin/openscad.AppImage
 echo "Checking OpenSCAD Accessible"
