9f7a5699db62e36c641a0a5dffbe2ebca60290ee
Trexdog319 <nbusedu@gmail.com>
2024-11-09 18:26:53 -0500

    Fix import paths for AppComponent in mission spec and main entry file

--- CBC-UI/angular-app/src/main.ts
+++ CBC-UI/angular-app/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/pages/main/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
