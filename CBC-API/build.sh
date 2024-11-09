76cf1e94fffa2d72e781180f7a2709a501f0318c
KrishGup <41498109+KrishGup@users.noreply.github.com>
2024-11-09 11:36:02 -0500

    Update build.sh to use sudo

--- CBC-API/build.sh
+++ CBC-API/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Updating package list..."
-apt-get update
+sudo apt-get update
 echo "Installing OpenSCAD..."
-apt-get install -y openscad
+sudo apt-get install -y openscad
 echo "Checking OpenSCAD version..."
 openscad --version
 echo "Installing Python dependencies..."
