174e6fe0d733a390449e9fbb0cce72ed6ec5c182
Trexdog319 <nbusedu@gmail.com>
2024-11-09 20:40:50 -0500

    Add Header component and global styles; update routing and app structure

--- CBC-UI/angular-app/src/app/app.routes.ts
+++ CBC-UI/angular-app/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { OrderStatusComponent } from './order-status/order-status.component';
 import { TdComponent } from './Td/Td.component';
 import {AboutComponent} from './about/about.component';
-import {TutorialComponent} from './Tutorials/Tutorials.component';
+import {TutorialsComponent} from './Tutorials/Tutorials.component';
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'order', component: OrderStatusComponent },
   { path: '3d', component: TdComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'tutorials', component: TutorialComponent }
+  { path: 'tutorials', component: TutorialsComponent },
+  { path: 'header', component: HeaderComponent }
 ];
--- CBC-UI/angular-app/src/main.ts
+++ CBC-UI/angular-app/src/main.ts
@@ -7,10 +7,11 @@ import { provideRouter } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule, FormsModule, ReactiveFormsModule, CommonModule),
+    importProvidersFrom(HttpClientModule, FormsModule, ReactiveFormsModule, CommonModule), provideAnimationsAsync(),
   ]
 }).catch(err => console.error(err));
